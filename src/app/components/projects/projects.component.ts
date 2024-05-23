7471aa8bf6fafa5d5e724b0429ce13c0e365cd23
Lattaruolo Andrea <lattaruolo.96@gmail.com>
2024-05-23 11:59:42 +0200

    scrolling projects testing

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,16 +1,17 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule,MatGridListModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
   // https://stackoverflow.com/questions/48955095/horizontal-scroll-using-buttons-on-angular2
-
+  // https://www.databricks.com/ example projects
   // @ViewChild('widgetsContent', { read: ElementRef }) 
   // private widgetsContent: ElementRef<any>;
 
