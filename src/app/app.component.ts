47341ac2e7f6b87409741fecd0a73c0c170088f6
Lattaruolo Andrea <lattaruolo.96@gmail.com>
2024-09-22 12:33:05 +0200

    To do fix contacts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ThemeService } from './services/theme.service.service';
 import { CommonModule } from '@angular/common';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent,CommonModule], //BrowserAnimationsModule,BrowserModule
+  // providers: [provideAnimations()],
+  imports: [RouterOutlet,HomeComponent,CommonModule], //B
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, ReactiveFormsModule, MatCardModule, MatInputModule, MatButtonModule,],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  emailForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.emailForm = this.fb.group({
+      to: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.emailForm.valid) {
+      console.log('Form submitted', this.emailForm.value);
+      // Here you would typically call a service to send the email
+      // For example: this.emailService.sendEmail(this.emailForm.value);
+    }
+  }
 }
